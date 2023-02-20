f37399bf66777265ed3cfd509f7ba1bb1abdd617
Kevin Massol <kevin.massol@icloud.com>
2023-02-20 11:35:44 +0100

    Initial Commit

---
+++ Aladin/AladinApp.swift
@@ -0,0 +1,17 @@
+//
+//  AladinApp.swift
+//  Aladin
+//
+//  Created by KEVIN on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AladinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
