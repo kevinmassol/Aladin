abed1ce140cf8374465ecbde8376c15dcd45e55c
Kevin Massol <kevin.massol@icloud.com>
2023-02-22 22:11:23 +0100

    [feat #2] : add images and texts in the HomeView

--- Aladin/Views/Components/HomeView.swift
+++ Aladin/Views/Components/HomeView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Hello")
+        VStack {
+            ZStack(alignment: .bottom) {
+                Image("cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 250)
+                Image("profile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 125, height: 125)
+                    .background(.white)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(.indigo, style: StrokeStyle(lineWidth: 3)))
+            }
+            Text("L'app du Génie")
+                .font(.title)
+                .foregroundColor(.accentColor)
+                .bold()
+            Divider()
+                .frame(height: 2)
+                .background(.indigo)
+                .padding(EdgeInsets(top: 0, leading: 18, bottom: 0, trailing: 18))
+            Text("Venez me frotter la lampe pour obtenir vos 3 voeux, et ainsi réaliser vos rêves les plus fous!")
+                .foregroundColor(.accentColor)
+                .italic()
+                .multilineTextAlignment(.center)
+                .padding()
+            Image("lamp")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 200)
+                .cornerRadius(25)
+                .padding()
+                .clipped()
+            Spacer()
+        }
+//        .edgesIgnoringSafeArea(.all)
     }
 }
 
