8e9853e124ca375e5d8ed6768258a5b9a1e8512e
Kevin Massol <kevin.massol@icloud.com>
2023-02-24 11:21:56 +0100

    [feat #4] : add images and texts in the ContactView

--- Aladin/Views/Components/ContactView.swift
+++ Aladin/Views/Components/ContactView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct ContactView: View {
+    
+    @State var buttonCliked = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Ou me trouver ?")
+                .font(.title)
+                .foregroundColor(.accentColor)
+                .bold()
+            Image("cave_outside")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Button("Dans la cave des merveilles") {
+                buttonCliked.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+            .sheet(isPresented: $buttonCliked) {
+                VStack {
+                    Text("Bien au chaud à l'intérieur de la lampe")
+                        .foregroundColor(.accentColor)
+                        .bold()
+                    Image("dans_lampe")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+            }
+            Image("cave_inside")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+                .shadow(color: .accentColor, radius: 2, x: 3, y: 3)
+                .padding()
+            Spacer()
+            Text("Attention !!! \nVous ne pouvez prendre QUE la lampe dans cette cave aux merveilles !")
+                .font(.caption)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .italic()
+        }
     }
 }
 
